bee099e733af481cb8b3d5ede2a2bf90f1d1535f
Briareos19 <hecantoquiros3@gmail.com>
2021-09-24 09:13:18 -0500

    nuevo crud

---
+++ HTML/Practica/app-test-example/__tests__/app.spec.js
@@ -0,0 +1,79 @@
+import { mount } from "@vue/test-utils";
+import App from "./../src/App.vue";
+
+describe("App", () => {
+  // Inspect the raw component options
+  it("has data", () => {
+    expect(typeof App.data).toBe("function");
+  });
+});
+
+describe("Mounted App", () => {
+  const wrapper = mount(App);
+
+  test("is a Vue instance", () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+});
+
+describe("Mounted App", () => {
+  const wrapper = mount(App);
+
+  it("renders the correct markup", () => {
+    expect(wrapper.html()).toContain("What is the sum of the two numbers?");
+  });
+});
+
+describe("Mounted App", () => {
+  const wrapper = mount(App);
+
+  it("has a button", () => {
+    expect(wrapper.contains("button")).toBe(true);
+  });
+});
+
+describe("Mounted App", () => {
+  const wrapper = mount(App);
+
+  it("renders correctly with different data", async () => {
+    wrapper.setData({ x1: 5, x2: 10 });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toContain("10");
+  });
+});
+
+describe("Mounted App", () => {
+  const wrapper = mount(App);
+
+  it("button click without correct sum", () => {
+    expect(wrapper.vm.message).toBe("");
+    const button = wrapper.find("button");
+    button.trigger("click");
+    expect(wrapper.vm.message).toBe("TRY AGAIN");
+  });
+});
+
+
+describe("Mounted App", () => {
+  const wrapper = mount(App);
+
+  it("button click with correct sum", () => {
+    expect(wrapper.vm.message).toBe("");
+    wrapper.setData({x1:5, x2:10, guess:15});
+    const button = wrapper.find("button");
+    button.trigger("click");
+    expect(wrapper.vm.message).toBe("SUCCESS!");
+  });
+});
+
+describe("Mounted App", () => {
+  const wrapper = mount(App);
+
+  it("button click with correct sum", () => {
+    expect(wrapper.vm.message).toBe("");
+    wrapper.setData({x1:25, x2:10, guess:40});
+    const button = wrapper.find("button");
+    button.trigger("click");
+    expect(wrapper.vm.message).toBe("TRY AGAIN");
+  });
+});
---
+++ HTML/Practica/vue-mevn-stack-app-master/README.md
@@ -0,0 +1,47 @@
+# vue-mevn-stack-app
+This is a step by step MEVN stack tutorial, in this tutorial, we are going to learn how to create MEVN stack app. (MongoDB, Express.js, Vue.js, Node.js).
+
+[Vue MEVN Stack Tutorial – Build Full Stack Vue.js CRUD App](https://www.positronx.io/vue-mevn-stack-tutorial-build-full-stack-vue-js-crud-app/)
+
+
+## Start the MEVN Stack project.
+```
+Git clone https://github.com/SinghDigamber/vue-mevn-stack-app.git
+
+Get inside the project
+cd vue-mevn-stack-app
+
+Install the required packages:
+npm install
+
+Start the vue app on http://localhost:8080
+npm run serve
+
+Get inside the Node server folder:
+cd backend
+
+Install the required packages:
+npm install
+
+Start the mongodb server.
+mongo
+
+Start node server on http://localhost:4000/api 
+nodemon
+
+```
+npm install bootstrap --save
+npm install vue-router --save
+
+como se va a usar un backend, hay que instalar axios
+npm install axios --save
+
+por otra parte para el servidor 
+    npm init
+	npm install express body-parser mongoose cors --save
+    //refresca el servidor automaticamente
+    npm install nodemon
+
+
+    el database.js ->archivo de conexion
+    models - schema
